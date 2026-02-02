159e94f354a16ba0ba36e47368f064bc90a8e844
nischalpraj <nischalpraj776@gmail.com>
2026-02-02 18:39:27 +0545

    Making dropdown

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
- const base_url = "https://api.frankfurter.dev/v1/latest";
+const base_url = "https://api.frankfurter.dev/v1/latest";
+ 
+const dropdown = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdown) {
+    for (code in countryList) {
+        let newoption = document.createElement("option");
+        newoption.innerText = currCode;
+        newoption.value = currCode;
+    }
+}
