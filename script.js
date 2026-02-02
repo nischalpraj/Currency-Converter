f36a33cf1bc357194a09fac23c3d76f6836a1c14
nischalpraj <nischalpraj776@gmail.com>
2026-02-02 18:41:06 +0545

    Added the country code

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const base_url = "https://api.frankfurter.dev/v1/latest";
 const dropdown = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdown) {
-    for (code in countryList) {
+    for (currCode in countryList) {
         let newoption = document.createElement("option");
         newoption.innerText = currCode;
         newoption.value = currCode;
+        select.append(newoption);
     }
 }
