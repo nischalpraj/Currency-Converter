b9d27afc71c8182252aafc01570cc844c10e4136
nischalpraj <nischalpraj776@gmail.com>
2026-02-03 18:01:39 +0545

    Fixed bug

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ const base_url = "https://api.frankfurter.dev/v1/latest";
 const dropdown = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdown) {
-  for (currCode in countryList) {
+  for ( let currCode in countryList) {
     let newoption = document.createElement("option");
     newoption.innerText = currCode;
     newoption.value = currCode;
     if (select.name == "from" && currCode === "USD") {
       newoption.selected = "selected";
-    }
-    else if (select.name == "to" && currCode === "NPR") {
+    } else if (select.name == "to" && currCode === "NPR") {
       newoption.selected = "selected";
     }
     select.append(newoption);
   }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+
+  const updateFlag = (element) => {
+    console.log(element);
+  };
+
 }
 
-
-select.addEventListener("change", (evt) => {
-  updateFlag(evt.target);
-})
-
-
-const updateFlag = () => {
-
-}
