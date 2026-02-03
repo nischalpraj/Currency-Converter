36aaf27cf5da636007a6e42026e3643ef78bddcb
nischalpraj <nischalpraj776@gmail.com>
2026-02-03 17:51:00 +0545

    Final commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const base_url = "https://api.frankfurter.dev/v1/latest";
- 
+
 const dropdown = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdown) {
-    for (currCode in countryList) {
-        let newoption = document.createElement("option");
-        newoption.innerText = currCode;
-        newoption.value = currCode;
-        select.append(newoption);
-    }
+  for (currCode in countryList) {
+    let newoption = document.createElement("option");
+    newoption.innerText = currCode;
+    newoption.value = currCode;
+    select.append(newoption);
+  }
 }
